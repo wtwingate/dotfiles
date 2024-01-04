d922f38b7cab7babb9204e6f7efab9211275395e
William Wingate <wtwingate@gmail.com>
2024-01-04 01:21:31 -0500

    Format init.lua with StyLua

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -232,7 +232,7 @@ vim.api.nvim_create_autocmd("LspAttach", {
 		vim.keymap.set({ "n", "v" }, "<Leader>ca", vim.lsp.buf.code_action, opts)
 		vim.keymap.set("n", "gr", vim.lsp.buf.references, opts)
 		vim.keymap.set("n", "<Leader>f", function()
-			vim.lsp.buf.format { async = true }
+			vim.lsp.buf.format({ async = true })
 		end, opts)
 	end,
 })
