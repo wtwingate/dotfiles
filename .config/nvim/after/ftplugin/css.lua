9e9e02178b2b8ef0464c64edc62e71cc41befb92
William Wingate <wtwingate@gmail.com>
2024-01-04 15:02:27 -0500

    Set options for various file types

---
+++ .config/nvim/after/ftplugin/css.lua
@@ -0,0 +1,4 @@
+-- CSS Settings
+vim.o.tabstop = 2
+vim.o.shiftwidth = 2
+vim.o.expandtab = true
---
+++ .config/nvim/after/ftplugin/javascript.lua
@@ -0,0 +1,4 @@
+-- JavaScript Settings
+vim.o.tabstop = 2
+vim.o.shiftwidth = 2
+vim.o.expandtab = true
