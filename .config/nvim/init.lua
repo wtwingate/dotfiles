a1478eb971b7038029e159d7153638bd829300f7
William Wingate <wtwingate@gmail.com>
2024-05-12 14:31:56 -0400

    Add Treesitter plugin for Neovim

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -95,5 +95,27 @@ require("lazy").setup({
 		config = function()
 			vim.cmd.colorscheme("catppuccin")
 		end
+	},
+
+	{
+		"nvim-treesitter/nvim-treesitter",
+		build = ":TSUpdate",
+		config = function()
+			require("nvim-treesitter.configs").setup({
+				ensure_installed = { "c", "lua", "vim", "vimdoc", "query" },
+				sync_install = false,
+				auto_install = true,
+				ignore_install = {},
+				highlight = {
+					enable = true,
+					disable = {},
+					additional_vim_regex_highlighting = false
+				},
+				indent = {
+					enable = true,
+					disable = {}
+				}
+			})
+		end
 	}
 })
