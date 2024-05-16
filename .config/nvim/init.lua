2c5264d261ea0520ae527e926da464f97f5a9102
William Wingate <wtwingate@gmail.com>
2024-05-16 15:09:24 -0400

    Install Tree-sitter plugin for Neovim

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -97,4 +97,36 @@ require("lazy").setup({
 			lspconfig.gopls.setup({})
 		end,
 	},
+
+	{
+		"nvim-treesitter/nvim-treesitter",
+		build = ":TSUpdate",
+		config = function()
+			require("nvim-treesitter.configs").setup({
+				ensure_installed = {
+					"bash",
+					"c",
+					"lua",
+					"markdown",
+					"markdown_inline",
+					"python",
+					"query",
+					"vim",
+					"vimdoc",
+				},
+				sync_install = false,
+				auto_install = true,
+				ignore_install = {},
+				highlight = {
+					enable = true,
+					disable = {},
+					additional_vim_regex_highlighting = false,
+				},
+				indent = {
+					enable = true,
+					disable = {},
+				},
+			})
+		end
+	}
 })
