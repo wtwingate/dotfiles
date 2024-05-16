c4787a6dd911a6c02d385f4b8db6b72c065df6e5
William Wingate <wtwingate@gmail.com>
2024-05-16 12:37:09 -0400

    Install lazy.nvim and LSP plugin for Neovim

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -69,3 +69,32 @@ vim.api.nvim_create_autocmd("TextYankPost", {
 		vim.highlight.on_yank()
 	end,
 })
+
+-- [[ Neovim Plugins ]]
+
+-- Install and bootstrap plugin manager
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+if not (vim.uv or vim.loop).fs_stat(lazypath) then
+	vim.fn.system({
+		"git",
+		"clone",
+		"--filter=blob:none",
+		"https://github.com/folke/lazy.nvim.git",
+		"--branch=stable", -- latest stable release
+		lazypath,
+	})
+end
+vim.opt.rtp:prepend(lazypath)
+
+-- Install and configure plugins
+require("lazy").setup({
+
+	{
+		"neovim/nvim-lspconfig",
+		dependencies = {},
+		config = function()
+			local lspconfig = require("lspconfig")
+			lspconfig.gopls.setup({})
+		end,
+	},
+})
