7282c08cdbf38694ca12b1b42cc18559549c368f
William Wingate <wtwingate@gmail.com>
2024-05-16 21:14:14 -0400

    Install fuzzy finder plugin for Neovim

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -136,5 +136,26 @@ require("lazy").setup({
 				},
 			})
 		end
-	}
+	},
+
+	{
+		"nvim-telescope/telescope.nvim",
+		branch = "0.1.x",
+		dependencies = {
+			"nvim-lua/plenary.nvim",
+			{
+				"nvim-telescope/telescope-fzf-native.nvim",
+				build = "make",
+			},
+		},
+		config = function()
+			require("telescope").setup({})
+			require("telescope").load_extension("fzf")
+			local builtin = require("telescope.builtin")
+			vim.keymap.set("n", "<Leader>ff", builtin.find_files, {})
+			vim.keymap.set("n", "<Leader>fg", builtin.live_grep, {})
+			vim.keymap.set("n", "<Leader>fb", builtin.buffers, {})
+			vim.keymap.set("n", "<Leader>fh", builtin.help_tags, {})
+		end
+	},
 })
