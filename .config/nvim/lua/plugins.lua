9a03dc0fe8e7bae9aacab9615eeb8b9bbbb7db84
William Wingate <wtwingate@gmail.com>
2024-05-24 23:34:52 -0400

    Remove a few plugins while we're at it

--- .config/nvim/lua/plugins.lua
+++ .config/nvim/lua/plugins.lua
@@ -198,20 +198,12 @@ return {
 
     {
         "nvim-lualine/lualine.nvim",
-        dependencies = { "nvim-tree/nvim-web-devicons" },
+        -- dependencies = { "nvim-tree/nvim-web-devicons" },
         opts = {
             options = {
                 icons_enabled = false,
-                component_separators = "|",
-                section_separators = "",
             },
         },
-    },
-
-    {
-        "lukas-reineke/indent-blankline.nvim",
-        main = "ibl",
-        opts = {},
     },
 
     {
