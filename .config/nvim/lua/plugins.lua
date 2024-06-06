4d45376d27714219306dd078c11e72aac41de9f2
William Wingate <wtwingate@gmail.com>
2024-06-06 00:05:22 -0400

    Use Catppuccin color scheme for Neovim

--- .config/nvim/lua/plugins.lua
+++ .config/nvim/lua/plugins.lua
@@ -199,11 +199,12 @@ return {
     },
 
     {
-        "ellisonleao/gruvbox.nvim",
+        "catppuccin/nvim",
+        name = "catppuccin",
         lazy = false,
         priority = 1000,
         config = function()
-            vim.cmd.colorscheme("gruvbox")
+            vim.cmd.colorscheme("catppuccin")
         end,
     },
 
