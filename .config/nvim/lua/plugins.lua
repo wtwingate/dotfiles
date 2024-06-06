3f82da8115b3dc53c011111cdb449e80bb3e7cd1
William Wingate <wtwingate@gmail.com>
2024-06-06 18:58:09 -0400

    Change color scheme and update plugins

--- .config/nvim/lua/plugins.lua
+++ .config/nvim/lua/plugins.lua
@@ -199,12 +199,11 @@ return {
     },
 
     {
-        "catppuccin/nvim",
-        name = "catppuccin",
+        "ellisonleao/gruvbox.nvim",
         lazy = false,
         priority = 1000,
         config = function()
-            vim.cmd.colorscheme("catppuccin")
+            vim.cmd.colorscheme("gruvbox")
         end,
     },
 
