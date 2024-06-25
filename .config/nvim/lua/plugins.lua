10a4320ca7dd8f52ff8dc16409778f2a1430b7ea
William Wingate <wwingate@horizoninvestments.com>
2024-06-25 09:21:01 -0400

    Swap out Clojure plugins for Neovim

--- .config/nvim/lua/plugins.lua
+++ .config/nvim/lua/plugins.lua
@@ -228,7 +228,29 @@ return {
     "tpope/vim-repeat",
 
     -- Clojure plugins
-    "tpope/vim-fireplace",
+    {
+        "Olical/conjure",
+        ft = { "clojure" }, -- etc
+        lazy = true,
+        init = function()
+            -- Set configuration options here
+            vim.g["conjure#debug"] = true
+        end,
+
+        -- Optional cmp-conjure integration
+        dependencies = { "PaterJason/cmp-conjure" },
+    },
+
+    {
+        "PaterJason/cmp-conjure",
+        lazy = true,
+        config = function()
+            local cmp = require("cmp")
+            local config = cmp.get_config()
+            table.insert(config.sources, { name = "conjure" })
+            return cmp.setup(config)
+        end,
+    },
     "guns/vim-sexp",
     "tpope/vim-sexp-mappings-for-regular-people",
 }
