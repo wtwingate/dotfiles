f1f9ea3b3a87da0d765cecd8c504545e16ef5eff
William Wingate <wwingate@horizoninvestments.com>
2024-07-02 15:58:45 -0400

    Swap out some Neovim plugins

--- .config/nvim/lua/plugins.lua
+++ .config/nvim/lua/plugins.lua
@@ -181,7 +181,7 @@ return {
                 html = { "prettier" },
                 javascript = { "prettier" },
                 lua = { "stylua" },
-                python = { "isort", "autopep8" },
+                -- python = { "isort", "autopep8" },
             },
             format_on_save = {
                 timeout_ms = 500,
@@ -222,32 +222,14 @@ return {
     },
 
     {
-        "Olical/conjure",
-        ft = { "clojure" }, -- etc
-        lazy = true,
-        init = function()
-            -- Set configuration options here
-            -- vim.g["conjure#debug"] = true
-        end,
-
-        -- Optional cmp-conjure integration
-        dependencies = { "PaterJason/cmp-conjure" },
-    },
-
-    {
-        "PaterJason/cmp-conjure",
-        lazy = true,
-        config = function()
-            local cmp = require("cmp")
-            local config = cmp.get_config()
-            table.insert(config.sources, { name = "conjure" })
-            return cmp.setup(config)
-        end,
+        "numToStr/Comment.nvim",
+        opts = {},
     },
 
     "tpope/vim-fugitive",
     "tpope/vim-surround",
     "tpope/vim-repeat",
+    "tpope/vim-fireplace",
     "guns/vim-sexp",
     "tpope/vim-sexp-mappings-for-regular-people",
 }
