ea33b68cf68efde4ed9a40297b7e68879804209c
William Wingate <wwingate@horizoninvestments.com>
2024-07-08 13:26:43 -0400

    Tweak color scheme options for Neovim

--- .config/nvim/lua/plugins.lua
+++ .config/nvim/lua/plugins.lua
@@ -211,6 +211,12 @@ return {
         lazy = false,
         priority = 1000,
         config = function()
+            require("catppuccin").setup({
+                background = {
+                    light = "latte",
+                    dark = "macchiato",
+                },
+            })
             vim.cmd.colorscheme("catppuccin")
         end,
     },
