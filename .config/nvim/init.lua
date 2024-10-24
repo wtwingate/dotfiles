5722300289b1ad392c752c9a6b902c04a04bf687
William Wingate <wtwingate@gmail.com>
2024-10-23 20:11:45 -0400

    Rewrite Vim config in Lua for Neovim

---
+++ .config/nvim/init.lua
@@ -0,0 +1,46 @@
+--
+--                             ________ ++     ________
+--                            /VVVVVVVV\++++  /VVVVVVVV\
+--                            \VVVVVVVV/++++++\VVVVVVVV/
+--                             |VVVVVV|++++++++/VVVVV/'
+--                             |VVVVVV|++++++/VVVVV/'
+--                            +|VVVVVV|++++/VVVVV/'+
+--                          +++|VVVVVV|++/VVVVV/'+++++
+--                        +++++|VVVVVV|/VVVVV/'+++++++++
+--                          +++|VVVVVVVVVVV/'+++++++++
+--                            +|VVVVVVVVV/'+++++++++
+--                             |VVVVVVV/'+++++++++
+--                             |VVVVV/'+++++++++
+--                             |VVV/'+++++++++
+--                             'V/'   ++++++
+--                                      ++
+--
+
+-- Enable line numbers
+vim.opt.number = true
+vim.opt.relativenumber = true
+
+-- Set indentation options
+vim.opt.tabstop = 8
+vim.opt.softtabstop = 8
+vim.opt.shiftwidth = 8
+vim.opt.expandtab = false
+
+-- Set cursor options
+vim.opt.cursorline = true
+vim.opt.scrolloff = 4
+vim.opt.sidescrolloff = 4
+
+-- Set line wrap options
+vim.opt.wrap = false
+vim.opt.linebreak = true
+vim.opt.breakindent = true
+
+-- Set search options
+vim.opt.hlsearch = true
+vim.opt.incsearch = true
+vim.opt.ignorecase = true
+vim.opt.smartcase = true
+
+-- Save undo history
+vim.opt.undofile = true
